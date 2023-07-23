6507a143322bafad6b4d90c694a5fbe41689ddb7
emart2991 <124415099+emart2991@users.noreply.github.com>
2023-07-23 12:32:08 -0400

    Initial
    Initial commit

---
+++ dateTimePractice/dateTimePractice/dateTimePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  dateTimePracticeApp.swift
+//  dateTimePractice
+//
+//  Created by Eric Martin on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct dateTimePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
